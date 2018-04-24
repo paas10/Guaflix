7e5d471ee4c916894f5a7921e4661483cb799c1d
JoshHC <davidhigueros@gmail.com>
2018-04-24 16:15:34 -0600

    Home Controller 100% Funcional

--- Guaflix_1104017_1169317/Guaflix_1104017_1169317/Controllers/HomeController.cs
+++ Guaflix_1104017_1169317/Guaflix_1104017_1169317/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Guaflix_1104017_1169317.Clases;
+using Libreria_de_Clases;
+using Newtonsoft.Json;
 
 namespace Guaflix_1104017_1169317.Controllers
 {
@@ -23,14 +26,16 @@ namespace Guaflix_1104017_1169317.Controllers
             if (collection["Username"] == null)
                 return View();
 
+
             Usuario usuarioEncontrado = DataBase.Instance.ArboldeUsuarios.Buscar(UserLogin);
 
             //Se inserta el nuevo usuario al Árbol de usuarios
-            if (usuarioEncontrado != null || (collection["Username"] == "admin" && collection["Password"] == "admin"))
+            if (usuarioEncontrado != null || (collection["Username"] == "admin" && collection["Password"] == "admin") || (collection["Username"] == "Admin" && collection["Password"] == "Admin"))
             {
                 if (usuarioEncontrado != null)
                 {
-                    ViewBag.Message = usuarioEncontrado.Nombre;
+                    var Mensaje = usuarioEncontrado.Nombre +" "+ usuarioEncontrado.Apellido;
+                    ViewBag.Message = Mensaje;
                     UserLogin.Logeado = true;
                 }
 
@@ -58,7 +63,7 @@ namespace Guaflix_1104017_1169317.Controllers
             else
             {
                 //Si no es correcto se envia un mensaje de Error
-                TempData["msg1"] = "<script> alert('El Usuario o La Contraseña es Incorrecta');</script>";
+                TempData["msg"] = "<script> alert('El Usuario o La Contraseña es Incorrecta');</script>";
                 return View();
             }
         }
@@ -76,7 +81,7 @@ namespace Guaflix_1104017_1169317.Controllers
                 Nuevo.Logeado = false;
 
                 DataBase.Instance.ArboldeUsuarios.Insertar(Nuevo);
-                TempData["msg"] = "<script> alert('Usuario insertado con éxito');</script>";
+                TempData["msg"] = "<script> alert('Usuario ingresado con éxito');</script>";
             }
             return View();
         }
@@ -91,7 +96,8 @@ namespace Guaflix_1104017_1169317.Controllers
             {
                 if (item.Logeado == true)
                 {
-                    ViewBag.Message = item.Nombre;
+                    var Mensaje = item.Nombre + item.Apellido;
+                    ViewBag.Message = Mensaje;
                 }
             }
 
@@ -101,22 +107,138 @@ namespace Guaflix_1104017_1169317.Controllers
 
         public ActionResult UsuarioDecision()
         {
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            //Se evalua la decision y se envia el nombre de usuario para mostrarlo en las vistas
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    var Mensaje = item.Nombre + item.Apellido;
+                    ViewBag.Message = Mensaje;
+                }
+            }
             return View();
         }
 
-
-        public ActionResult About()
+        public ActionResult MiUsuario()
         {
-            ViewBag.Message = "Your application description page.";
+            //Se crea un nuevo Usuario
+            Usuario Nuevo = new Usuario();
+            //Se crea una lista temporal de usuarios y una lista de usuarios para poder pasarla posteriormente.
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            List<Usuario> ListadeUsuarios = new List<Usuario>();
 
+            //a la lista temporal de usuarios se le asignan los usuarios que posee el arbol.
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            //Se busca el usuario que esta logeado y es el que se envia a la vista.
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    Nuevo = item;
+                    ViewBag.Message = Nuevo.Nombre;
+                    ListadeUsuarios.Add(Nuevo);
+                }
+            }
+            return View(ListadeUsuarios);
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            //Se deslogea al usuario que coincide con las especificaciones
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    item.Logeado = false;
+                }
+            }
+            return View("Index");
+        }
+
+        public ActionResult UploadFile()
+        {
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            //Se crea una lista temporal de usuarios para ver si esta logeada o no
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    ViewBag.Message = item.Nombre;
+                }
+            }
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Aca se hace el Ingreso por medio de Archivo de Texto, ya que el Boton de Result esta Linkeado.
+        public ActionResult UploadFile(HttpPostedFileBase file, int? Tipo)
         {
-            ViewBag.Message = "Your contact page.";
+            //Se valida el .json
+            if (Path.GetExtension(file.FileName) != ".json")
+            {
+                //Aca se debe de Agregar una Vista de Error, o de Datos No Cargados
+                TempData["msg"] = "<script> alert('Error El Archivo Cargado No es de Tipo Json');</script>";
+                return RedirectToAction("Error", "Shared");
+            }
 
-            return View();
+            Stream Direccion = file.InputStream;
+            //Se lee el Archivo que se subio, por medio del Lector
+
+            StreamReader Lector = new StreamReader(Direccion, System.Text.Encoding.UTF8);
+            //El Archivo se lee en una linea para luego ingresarlo
+
+            string Dato = "";
+            Dato = Lector.ReadToEnd();
+
+            //Se deserealiza el objeto por medio de .json obteniendo una lista de peliculas
+            var ListadePeliculasGeneral = JsonConvert.DeserializeObject<List<Pelicula>>(Dato);
+
+            //Se insertan las peliculas en el arbol y se clasifican
+            foreach (var item in ListadePeliculasGeneral)
+            {
+                if (item.Tipo == "Pelicula")
+                {
+                    DataBase.Instance.ArboldePeliculasPorNombre.Insertar(item);
+                    DataBase.Instance.ArboldePeliculasPorGenero.Insertar(item);
+                    DataBase.Instance.ArboldePeliculasPorAño.Insertar(item);
+                }
+                else if (item.Tipo == "Serie")
+                {
+                    DataBase.Instance.ArboldeSeriesPorNombre.Insertar(item);
+                    DataBase.Instance.ArboldeSeriesPorGenero.Insertar(item);
+                    DataBase.Instance.ArboldeSeriesPorAño.Insertar(item);
+                }
+                else if (item.Tipo == "Documental")
+                {
+                    DataBase.Instance.ArboldeDocumentalesPorNombre.Insertar(item);
+                    DataBase.Instance.ArboldeDocumentalesPorGenero.Insertar(item);
+                    DataBase.Instance.ArboldeDocumentalesPorAño.Insertar(item);
+                }
+            }
+
+            //Se crea una lista temporal de usuarios para identificar cual esta logeado
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    ViewBag.Message = item.Nombre;
+                }
+            }
+
+            return View("MenudeDecision");
         }
     }
 }
--- Guaflix_1104017_1169317/Guaflix_1104017_1169317/Controllers/PeliculaController.cs
+++ Guaflix_1104017_1169317/Guaflix_1104017_1169317/Controllers/PeliculaController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Guaflix_1104017_1169317.Clases;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,12 +17,370 @@ namespace Guaflix_1104017_1169317.Controllers
             return View();
         }
 
-        // GET: Pelicula/Details/5
-        public ActionResult Details(int id)
+        public void imprimirArchivo()
         {
+
+            StreamWriter escritor = new StreamWriter(@"C:\Users\Admin\Desktop\Bitacora.txt");
+
+            foreach (var linea in DataBase.Instance.ArchivoTexto)
+            {
+                escritor.WriteLine(linea);
+            }
+            escritor.Close();
+        }
+
+        // GET: Pelicula
+        public ActionResult MisPeliculas(string NombreUsuario, List<Pelicula> Lista)
+        {
+            //Se crean listas temporales de usuarios y peliculas para usarlas despues
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            List<Pelicula> ListaTemporaldePeliculas = new List<Pelicula>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            foreach (var item in DataBase.Instance.ArboldeSeriesPorNombre.ObtenerArbol())
+            {
+                if (item != null)
+                {
+                    ListaTemporaldePeliculas.Add(item);
+                }
+            }
+
+            foreach (var item in DataBase.Instance.ArboldeDocumentalesPorNombre.ObtenerArbol())
+            {
+                if (item != null)
+                {
+                    ListaTemporaldePeliculas.Add(item);
+                }
+            }
+
+            foreach (var item in DataBase.Instance.ArboldePeliculasPorNombre.ObtenerArbol())
+            {
+                if (item != null)
+                {
+                    ListaTemporaldePeliculas.Add(item);
+                }
+            }
+
+            //se evalua si el usuario esta logeado
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    var Mensaje = item.Nombre + " "+ item.Apellido;
+                    ViewBag.Message = Mensaje;
+                }
+            }
+            if (ListaTemporaldePeliculas == null)
+            {
+                //Se envia un mensaje de error si el usuario no tiene peliculas agregadas
+                TempData["msg"] = "<script> alert('No Tienes Peliculas Agregadas, Porfavor Agrega Peliculas Antes para ver tu Inicio');</script>";
+                return RedirectToAction("MenudeDecision", "Home");
+            }
+            else
+            {
+                if (Lista != null)
+                {
+                    return View(Lista);
+                }
+                //Se envia la lista temporal de peliculas a la vista
+                return View(ListaTemporaldePeliculas);
+            }
+        }
+
+        [ValidateInput(false)]
+        public ActionResult VerPelicula(string URL, string Trailer, string Nombre, string Tipo, string Genero, string Anio)
+        {
+            //Se Crea una lista temporal de usuario para evaluar cual esta logeado
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    var Mensaje = item.Nombre + " " + item.Apellido;
+                    ViewBag.Message = Mensaje;
+                }
+            }
+
+            //Se manda la lista especial a la vista para poder visualizar la unica pelicula
+            Pelicula NuevaPelicula = new Pelicula(URL, Trailer, Nombre, Tipo, Anio, Genero);
+            List<Pelicula> ListadePeliculas = new List<Pelicula>();
+            ListadePeliculas.Add(NuevaPelicula);
+            Session.Add("URL", NuevaPelicula.Trailer);
+            return View(ListadePeliculas);
+        }
+
+        public ActionResult BuscarPelicula(string Tipo, string Search)
+        {
+            //Se Crea una lista temporal de usuario para evaluar cual esta logeado
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            List<Pelicula> ListaTemporaldePeliculas = new List<Pelicula>();
+            List<Pelicula> ListaTemporaldeSeries = new List<Pelicula>();
+            List<Pelicula> ListaTemporaldeDocumentales = new List<Pelicula>();
+            List<Pelicula> ListaGeneral = new List<Pelicula>();
+
+            ListaTemporaldePeliculas = DataBase.Instance.ArboldePeliculasPorNombre.ObtenerArbol();
+            ListaTemporaldeSeries = DataBase.Instance.ArboldeSeriesPorNombre.ObtenerArbol();
+            ListaTemporaldeDocumentales = DataBase.Instance.ArboldeDocumentalesPorNombre.ObtenerArbol();
+
+            if (Tipo == "Nombre" && Search != null)
+            {
+                foreach (var item in ListaTemporaldePeliculas)
+                {
+                    if (item.Nombre == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+
+                foreach (var item in ListaTemporaldeSeries)
+                {
+                    if (item.Nombre == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+
+                foreach (var item in ListaTemporaldeDocumentales)
+                {
+                    if (item.Nombre == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+            }
+            else if (Tipo == "AniodeLanzamiento" && Search != null)
+            {
+                foreach (var item in ListaTemporaldePeliculas)
+                {
+                    if (item.AniodeLanzamiento == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+
+                foreach (var item in ListaTemporaldeSeries)
+                {
+                    if (item.AniodeLanzamiento == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+
+                foreach (var item in ListaTemporaldeDocumentales)
+                {
+                    if (item.AniodeLanzamiento == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+            }
+            else if (Tipo == "Genero" && Search != null)
+            {
+                foreach (var item in ListaTemporaldePeliculas)
+                {
+                    if (item.Genero == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+
+                foreach (var item in ListaTemporaldeSeries)
+                {
+                    if (item.Genero == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+
+                foreach (var item in ListaTemporaldeDocumentales)
+                {
+                    if (item.Genero == Search)
+                    {
+                        ListaGeneral.Add(item);
+                    }
+                }
+            }
+            else if (ListaGeneral.Count() == 0)
+            {
+                if (Tipo != null && Search != null)
+                {
+                    TempData["msg"] = "<script> alert('El Dato que Buscas no Existe');</script>";
+                }
+                else
+                {
+                    return View();
+                }
+            }
+
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    ViewBag.Message = item.Nombre;
+                }
+            }
+
+            return View("MisPeliculas", ListaGeneral);
+        }
+
+        public ActionResult json()
+        {
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            //Se crea una lista temporal de usuarios para ver si esta logeada o no
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    var Mensaje = item.Nombre + " " + item.Apellido;
+                    ViewBag.Message = Mensaje;
+                }
+            }
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Aca se hace el Ingreso por medio de Archivo de Texto, ya que el Boton de Result esta Linkeado.
+        public ActionResult json(HttpPostedFileBase file, int? Tipo)
+        {
+            //Se valida el .json
+            if (Path.GetExtension(file.FileName) != ".json")
+            {
+                //Aca se debe de Agregar una Vista de Error, o de Datos No Cargados
+                TempData["msg"] = "<script> alert('Error El Archivo Cargado No es de Tipo Json');</script>";
+                return RedirectToAction("Error", "Shared");
+            }
+
+            Stream Direccion = file.InputStream;
+            //Se lee el Archivo que se subio, por medio del Lector
+
+            StreamReader Lector = new StreamReader(Direccion, System.Text.Encoding.UTF8);
+            //El Archivo se lee en una linea para luego ingresarlo
+
+            string Dato = "";
+            Dato = Lector.ReadToEnd();
+
+            //Se deserealiza el objeto por medio de .json obteniendo una lista de peliculas
+            var ListadeUsuarios = JsonConvert.DeserializeObject<List<Usuario>>(Dato);
+
+            //Se insertan las peliculas en el arbol y se clasifican
+            foreach (var item in ListadeUsuarios)
+            {
+                DataBase.Instance.ArboldeUsuarios.Insertar(item);
+            }
+
+            //Se crea una lista temporal de usuarios para identificar cual esta logeado
+            List<Usuario> ListaTemporaldeUsuarios = new List<Usuario>();
+            ListaTemporaldeUsuarios = DataBase.Instance.ArboldeUsuarios.ObtenerArbol();
+
+            foreach (var item in ListaTemporaldeUsuarios)
+            {
+                if (item.Logeado == true)
+                {
+                    ViewBag.Message = item.Nombre;
+                }
+            }
+
+            return View("MiUsuario", "Home");
+        }
+
+        public ActionResult EliminarPelis()
+        {
+            List<Pelicula> ListadePeliculas = new List<Pelicula>();
+
+            foreach (var item in DataBase.Instance.ArboldeSeriesPorNombre.ObtenerArbol())
+            {
+                if (item != null)
+                {
+                    ListadePeliculas.Add(item);
+                }
+            }
+
+            foreach (var item in DataBase.Instance.ArboldeDocumentalesPorAño.ObtenerArbol())
+            {
+                if (item != null)
+                {
+                    ListadePeliculas.Add(item);
+                }
+            }
+
+            foreach (var item in DataBase.Instance.ArboldePeliculasPorNombre.ObtenerArbol())
+            {
+                if (item != null)
+                {
+                    ListadePeliculas.Add(item);
+                }
+            }
+
+
+            return View(ListadePeliculas);
+        }
+
+        // GET: Pelicula/Delete/5
+        public ActionResult Delete(string URL, string Trailer, string Nombre, string Tipo, string Anio, string Genero)
+        {
+            List<Pelicula> Lista = new List<Pelicula>();
+            Pelicula Nueva = new Pelicula(URL, Trailer, Nombre, Tipo, Anio, Genero);
+            Lista.Add(Nueva);
+            return View(Lista);
+        }
+
+        // POST: Pelicula/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string Nombre, FormCollection collection)
+        {
+            try
+            {
+                List<Pelicula> PeliculasEliminadas = new List<Pelicula>();
+                foreach (var item in DataBase.Instance.ArboldeSeriesPorNombre.ObtenerArbol())
+                {
+                    if (item != null)
+                    {
+                        PeliculasEliminadas.Add(item);
+                    }
+                }
+
+                foreach (var item in DataBase.Instance.ArboldeDocumentalesPorNombre.ObtenerArbol())
+                {
+                    if (item != null)
+                    {
+                        PeliculasEliminadas.Add(item);
+                    }
+                }
+
+                foreach (var item in DataBase.Instance.ArboldePeliculasPorNombre.ObtenerArbol())
+                {
+                    if (item != null)
+                    {
+                        PeliculasEliminadas.Add(item);
+                    }
+                }
+
+                foreach (var item in PeliculasEliminadas)
+                {
+                    if (item.Nombre == Nombre)
+                    {
+                        PeliculasEliminadas.Remove(item);
+                    }
+
+                }
+
+
+                return RedirectToAction("MisPeliculas", PeliculasEliminadas);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+       
         // GET: Pelicula/Create
         public ActionResult Create()
         {
@@ -64,26 +425,6 @@ namespace Guaflix_1104017_1169317.Controllers
             }
         }
 
-        // GET: Pelicula/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Pelicula/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+       
     }
 }
